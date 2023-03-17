84740c12f5519bd3f408dae9a8fd220817122abe
mostlikely4r <sebastiaan.keek@gmail.com>
2023-03-17 16:23:22 +0100

    -Added support for name/level threat api

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -10755,6 +10755,10 @@ void Unit::SendThreatUpdate()
         SendMessageToSet(data.str(), false);
         data.clear();
         data.str(std::string());
+        data << number << ": " << GetName() << GetLevel() << std::dec;
+        SendMessageToSet(data.str(), false);
+        data.clear();
+        data.str(std::string());
         data << number << ": " << uint32(count);
         SendMessageToSet(data.str(), false);
         data.clear();
@@ -10762,6 +10766,10 @@ void Unit::SendThreatUpdate()
         for (auto itr : tlist)
         {
             data << number << ": " << std::uppercase << "0x" << std::setfill('0') << std::setw(16) << std::hex << itr->getUnitGuid() << std::dec;
+            SendMessageToSet(data.str(), false);
+            data.clear();
+            data.str(std::string());
+            data << number << ": " << itr->getTarget()->GetName() << itr->getTarget()->GetLevel() << std::dec;
             SendMessageToSet(data.str(), false);
             data.clear();
             data.str(std::string());
@@ -10775,7 +10783,7 @@ void Unit::SendThreatUpdate()
     }
 }
 
-void Unit::SendHighestThreatUpdate(HostileReference* pHostilReference)
+void Unit::SendHighestThreatUpdate(HostileReference* pHostileReference)
 {
     ThreatList const& tlist = getThreatManager().getThreatList();
     if (uint32 count = tlist.size())
@@ -10791,7 +10799,15 @@ void Unit::SendHighestThreatUpdate(HostileReference* pHostilReference)
         SendMessageToSet(data.str(), false);
         data.clear();
         data.str(std::string());
-        data << number << ": " << std::uppercase << "0x" << std::setfill('0') << std::setw(16) << std::hex << pHostilReference->getUnitGuid() << std::dec;
+        data << number << ": " << GetName() << GetLevel() << std::dec;
+        SendMessageToSet(data.str(), false);
+        data.clear();
+        data.str(std::string());
+        data << number << ": " << std::uppercase << "0x" << std::setfill('0') << std::setw(16) << std::hex << pHostileReference->getUnitGuid() << std::dec;
+        SendMessageToSet(data.str(), false);
+        data.clear();
+        data.str(std::string());
+        data << number << ": " << pHostileReference->getTarget()->GetName() << pHostileReference->getTarget()->GetLevel() << std::dec;
         SendMessageToSet(data.str(), false);
         data.clear();
         data.str(std::string());
@@ -10802,6 +10818,10 @@ void Unit::SendHighestThreatUpdate(HostileReference* pHostilReference)
         for (auto itr : tlist)
         {
             data << number << ": " << std::uppercase << "0x" << std::setfill('0') << std::setw(16) << std::hex << itr->getUnitGuid() << std::dec;
+            SendMessageToSet(data.str(), false);
+            data.clear();
+            data.str(std::string());
+            data << number << ": " << itr->getTarget()->GetName() << itr->getTarget()->GetLevel() << std::dec;
             SendMessageToSet(data.str(), false);
             data.clear();
             data.str(std::string());
@@ -10828,6 +10848,10 @@ void Unit::SendThreatClear() const
     SendMessageToSet(data.str(), false);
     data.clear();
     data.str(std::string());
+    data << number << ": " << GetName() << GetLevel() << std::dec;
+    SendMessageToSet(data.str(), false);
+    data.clear();
+    data.str(std::string());
     data << number << ": END";
     SendMessageToSet(data.str(), false);
 }
@@ -10845,7 +10869,15 @@ void Unit::SendThreatRemove(HostileReference* pHostileReference) const
     SendMessageToSet(data.str(), false);
     data.clear();
     data.str(std::string());
+    data << number << ": " << GetName() << GetLevel() << std::dec;
+    SendMessageToSet(data.str(), false);
+    data.clear();
+    data.str(std::string());
     data << number << ": " << std::uppercase << "0x" << std::setfill('0') << std::setw(16) << std::hex << pHostileReference->getUnitGuid() << std::dec;
+    SendMessageToSet(data.str(), false);
+    data.clear();
+    data.str(std::string());
+    data << number << ": " << pHostileReference->getTarget()->GetName() << pHostileReference->getTarget()->GetLevel() << std::dec;
     SendMessageToSet(data.str(), false);
     data.clear();
     data.str(std::string());
