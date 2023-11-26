abeb8af44cd5b0a7a913e1b3fe13c4bd4fab1cb8
celguar <celguar@gmail.com>
2023-11-26 14:52:19 +0300

    update city title mod

--- src/game/Globals/ObjectMgr.cpp
+++ src/game/Globals/ObjectMgr.cpp
@@ -3642,7 +3642,7 @@ void ObjectMgr::SetCityRanks()
 
     for (uint8 i = 1; i < MAX_RACES; ++i)
     {
-        QueryResult* result = CharacterDatabase.PQuery("SELECT `guid`, `honor_standing` FROM `characters` WHERE `honor_standing` > 0 and `race` = %u ORDER BY `honor_standing` ASC LIMIT 1", i);
+        auto result = CharacterDatabase.PQuery("SELECT `guid`, `honor_standing` FROM `characters` WHERE `honor_standing` > 0 and `race` = %u ORDER BY `honor_standing` ASC LIMIT 1", i);
 
         if (result)
         {
@@ -3654,7 +3654,6 @@ void ObjectMgr::SetCityRanks()
 
                 highestStandingInRace[i] = std::make_pair(guid, honorStanding);
             } while (result->NextRow());
-            delete result;
         }
     }
 
